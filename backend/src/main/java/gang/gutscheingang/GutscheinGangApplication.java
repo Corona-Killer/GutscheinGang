31c9d5aadf1eeff9f7eb63b2e937dcf3d4150ada
Stefan Stumpf <stefan.stumpf@roth-gruppe.de>
2020-03-20 22:21:13 +0100

    Init backend

---
+++ backend/src/main/java/gang/gutscheingang/GutscheinGangApplication.java
@@ -0,0 +1,13 @@
+package gang.gutscheingang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutscheinGangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutscheinGangApplication.class, args);
+	}
+
+}
