d4cf47d02d75c3aac011a6c989b6f346d141801b
Stefan Stumpf <stefan.stumpf94@gmail.com>
2020-03-22 17:28:56 +0100

    frontend: Search Bar container fluid

--- app/src/components/pages/Home.tsx
+++ app/src/components/pages/Home.tsx
@@ -49,9 +49,9 @@ class Home extends Component<Props> {
 						<h1 className="text-white text-center">
 							Suche nach einem Unternehmen in Deiner Stadt!
 						</h1>
-						<div className="mt-5">
+						<div className="mt-5 w-100">
 							{/* <Form.Group controlId="123"> */}
-							<InputGroup>
+							<InputGroup className="jumbatron--search">
 								<AutoCompleteInput
 									suggestions={sectors.map((sector) => sector.name)}
 									type="text"
@@ -59,14 +59,6 @@ class Home extends Component<Props> {
 									placeholder="z.B. Lebensmittelgeschäft"
 									onChange={this.onChangeSearch}
 								/>
-								{/* <Form.Control type="text" style={{ height: '40px', fontSize: '1.2rem'}} placeholder="z.B. Lebensmittelgeschäft" onKeyDown={this.onKeyDown} /> */}
-								<InputGroup.Append>
-									<InputGroup.Text
-										style={{ backgroundColor: '#fff', borderLeft: '0px' }}
-									>
-										<Icons.Search />
-									</InputGroup.Text>
-								</InputGroup.Append>
 							</InputGroup>
 							{/* </Form.Group> */}
 						</div>
