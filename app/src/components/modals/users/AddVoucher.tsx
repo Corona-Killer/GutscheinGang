0e0926c5c166b08ace3af5c668fd3d1807aa9a15
Tobias Wälde <tobias.waelde@gmail.com>
2020-03-24 15:33:47 +0100

    Fix 'AddVoucher' modal

--- app/src/components/modals/users/AddVoucher.tsx
+++ app/src/components/modals/users/AddVoucher.tsx
@@ -1,194 +1,173 @@
-import React, { Component } from "react";
-import {
-  Modal,
-  Button,
-  Form,
-  Alert,
-  Row,
-  Col,
-  InputGroup
-} from "react-bootstrap";
-import AutoCompleteInput from "../../util/AutoCompleteInput/AutoCompleteInput";
-import { StoreState } from "../../../store";
-import { SectorsState } from "../../../store/models/sectors/reducer";
-import { connect } from "react-redux";
-var PayPalButton: any = require('react-paypal-button-v2');
+import React, { Component } from 'react';
+import { Modal, Button, Form, Row, Col, InputGroup } from 'react-bootstrap';
+import { StoreState } from '../../../store';
+import { SectorsState } from '../../../store/models/sectors/reducer';
+import { connect } from 'react-redux';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { toast } from 'react-toastify';
 
 const mapStateToProps = (state: StoreState) => ({
-  sectors: state.sectors
+	sectors: state.sectors
 });
 
 interface Props {
-  sectors: SectorsState;
+	sectors: SectorsState;
 }
 interface State {
-  modalOpen: boolean;
+	modalOpen: boolean;
+	amountInEuro: number;
 }
 
+const initState: State = {
+	modalOpen: false,
+	amountInEuro: 10
+};
+
 class AddVoucher extends Component<Props, State> {
-  //SECTION React lifecycle
+	//SECTION React lifecycle
 
-  amountInEuro: number = 10;
+	constructor(props: Props) {
+		super(props);
+		this.state = initState;
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      modalOpen: false
-    };
+		// Bind refs
+		// TODO
+	}
 
-    console.log();
+	//!SECTION
 
-    // Bind refs
-    // TODO
-  }
+	//SECTION Modal functions
 
-  //!SECTION
+	openModal = () => {
+		this.setState({ modalOpen: true });
+	};
+	closeModal = () => {
+		this.setState({ modalOpen: false });
+	};
 
-  //SECTION Modal functions
+	//!SECTION
 
-  openModal = () => {
-    this.setState({ modalOpen: true });
-  };
-  closeModal = () => {
-    this.setState({ modalOpen: false });
-  };
+	handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(e);
+	};
 
-  //!SECTION
+	onPaypalSuccess = (details: any, data: any) => {
+		toast.success('Transaction completed by ' + details.payer.name.given_name);
 
-  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => { 
-	  
-	e.preventDefault();
-	console.log(e);};
+		// OPTIONAL: Call your server to save the transaction
+		// return fetch('/paypal-transaction-complete', {
+		// 	method: 'post',
+		// 	body: JSON.stringify({
+		// 		orderID: data.orderID
+		// 	})
+		// });
+	};
 
-  render() {
-    const { modalOpen } = this.state;
+	render() {
+		const { modalOpen, amountInEuro } = this.state;
 
-    return (
-      <React.Fragment>
-        <Button
-          variant="primary"
-          onClick={this.openModal}
-          className="d-block ml-auto mb-3"
-        >
-          Gutschein kaufen
-        </Button>
+		return (
+			<React.Fragment>
+				<Button variant="primary" onClick={this.openModal} className="d-block ml-auto mb-3">
+					Gutschein kaufen
+				</Button>
 
-        <Modal show={modalOpen} centered onHide={this.closeModal} size="lg">
-          <Modal.Header className="bg-dark text-light">
-            <Modal.Title>Einen Gutschein kaufen</Modal.Title>
-          </Modal.Header>
-          <Form autoComplete="off" onSubmit={this.handleSubmit}>
-            <Modal.Body>
-              {/*<Alert variant="danger">ERROR: Some error message...</Alert>*/}
+				<Modal show={modalOpen} centered onHide={this.closeModal} size="lg">
+					<Modal.Header className="bg-dark text-light">
+						<Modal.Title>Einen Gutschein kaufen</Modal.Title>
+					</Modal.Header>
+					<Form autoComplete="off" onSubmit={this.handleSubmit}>
+						<Modal.Body>
+							{/*<Alert variant="danger">ERROR: Some error message...</Alert>*/}
 
-              <Row>
-                {/* Name */}
-                <Col md={6} xs={12}>
-                  <Form.Group controlId="firstName">
-                    <Form.Label>
-                      <b>
-                        Vorname<span className="text-danger">*</span>
-                      </b>
-                    </Form.Label>
-                    <Form.Control type="text" placeholder="z.B. Max" />
-                  </Form.Group>
-                </Col>
-                {/* City */}
-                <Col md={6} xs={12}>
-                  <Form.Group controlId="lastName">
-                    <Form.Label>
-                      <b>
-                        Nachname<span className="text-danger">*</span>
-                      </b>
-                    </Form.Label>
-                    <Form.Control type="text" placeholder="z.B. Mustermann" />
-                  </Form.Group>
-                </Col>
+							<Row>
+								{/* FirstName */}
+								<Col md={6} xs={12}>
+									<Form.Group controlId="firstName">
+										<Form.Label>
+											<b>
+												Vorname<span className="text-danger">*</span>
+											</b>
+										</Form.Label>
+										<Form.Control type="text" placeholder="z.B. Max" />
+									</Form.Group>
+								</Col>
+								{/* LastName */}
+								<Col md={6} xs={12}>
+									<Form.Group controlId="lastName">
+										<Form.Label>
+											<b>
+												Nachname<span className="text-danger">*</span>
+											</b>
+										</Form.Label>
+										<Form.Control type="text" placeholder="z.B. Mustermann" />
+									</Form.Group>
+								</Col>
 
-                {/* Sector */}
-                <Col xs={12}>
-                  <Form.Group controlId="sector">
-                    <Form.Label>
-                      <b>
-                        Email<span className="text-danger">*</span>
-                      </b>
-                    </Form.Label>
-                    {/* <Form.Control type="text" max={255} /> */}
-                    <Form.Control
-                      type="email"
-                      placeholder="z.B. max.mustermann@example.org"
-                    />
-                  </Form.Group>
-                </Col>
+								{/* Email */}
+								<Col xs={12}>
+									<Form.Group controlId="sector">
+										<Form.Label>
+											<b>
+												Email<span className="text-danger">*</span>
+											</b>
+										</Form.Label>
+										{/* <Form.Control type="text" max={255} /> */}
+										<Form.Control
+											type="email"
+											placeholder="z.B. max.mustermann@example.org"
+										/>
+									</Form.Group>
+								</Col>
 
-                <Col xs={12} className="mt-36">
-                  <Form.Group controlId="sector">
-                    <Form.Label>
-                      <b>
-                        Betrag<span className="text-danger">*</span>
-                      </b>
-                    </Form.Label>
-                    {/* <Form.Control type="text" max={255} /> */}
-                    <InputGroup className="mb-3">
-                      <Form.Control
-                        type="number"
-                        min={0}
-                        max={50}
-						step={1}
-							
-                        placeholder="z.B. 10 Euro"
-                      />
-                      <InputGroup.Append>
-                        <InputGroup.Text id="euroAddon">€</InputGroup.Text>
-                      </InputGroup.Append>
-                    </InputGroup>
-                  </Form.Group>
-                </Col>
-              </Row>
+								{/* Amount */}
+								<Col xs={12} className="mt-36">
+									<Form.Group controlId="sector">
+										<Form.Label>
+											<b>
+												Betrag<span className="text-danger">*</span>
+											</b>
+										</Form.Label>
+										<InputGroup className="mb-3">
+											<Form.Control
+												type="number"
+												min={0}
+												max={50}
+												step={1}
+												placeholder="z.B. 10 Euro"
+											/>
+											<InputGroup.Append>
+												<InputGroup.Text id="euroAddon">€</InputGroup.Text>
+											</InputGroup.Append>
+										</InputGroup>
+									</Form.Group>
+								</Col>
+							</Row>
 
-              <Row className="justify-content-md-center">
-                <Col xs={8} className="mt-36">
-                  <PayPalButton.PayPalButton
-                    amount={this.amountInEuro}
-                    // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
-                    onSuccess={(details: any, data: any) => {
-                      alert(
-                        "Transaction completed by " +
-                          details.payer.name.given_name
-                      );
-
-                      // OPTIONAL: Call your server to save the transaction
-                      return fetch("/paypal-transaction-complete", {
-                        method: "post",
-                        body: JSON.stringify({
-                          orderID: data.orderID
-                        })
-                      });
-                    }}
-                  />
-                </Col>
-              </Row>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="outline-danger"
-                type="reset"
-                onClick={this.closeModal}
-              >
-                Abbrechen
-              </Button>
-			  <Button
-                variant="success"
-                type="submit"
-              >
-                Bezahlen
-              </Button>
-            </Modal.Footer>
-          </Form>
-        </Modal>
-      </React.Fragment>
-    );
-  }
+							<Row className="justify-content-md-center">
+								<Col xs={8} className="mt-36">
+									<PayPalButton.PayPalButton
+										amount={amountInEuro}
+										// shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+										onSuccess={this.onPaypalSuccess}
+									/>
+								</Col>
+							</Row>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button variant="outline-danger" type="reset" onClick={this.closeModal}>
+								Abbrechen
+							</Button>
+							<Button variant="success" type="submit">
+								Bezahlen
+							</Button>
+						</Modal.Footer>
+					</Form>
+				</Modal>
+			</React.Fragment>
+		);
+	}
 }
 
 export default connect(mapStateToProps)(AddVoucher);
--- app/src/components/modals/users/react-paypal-button-v2.d.ts
+++
@@ -1,4 +0,0 @@
-export declare module 'react-paypal-button-v2' {
-    var PayPalButton: any;
-    export default PayPalButton;
-};
