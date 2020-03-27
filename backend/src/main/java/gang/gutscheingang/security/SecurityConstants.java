7f5b153a78d7ce61d82d2adda6ca4523a0da3175
redalertexpert <felix.rittler@outlook.com>
2020-03-27 02:09:21 +0100

    backend: added jwt secret env variable to README, now use env variable as jwt secret, added password constraints

--- backend/src/main/java/gang/gutscheingang/security/SecurityConstants.java
+++ backend/src/main/java/gang/gutscheingang/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package gang.gutscheingang.security;
 
 public class SecurityConstants {
-    public static final String SECRET = "SecretKeyToGenJWTs";
+    public static final String SECRET = System.getenv("JWT_SECRET");
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
